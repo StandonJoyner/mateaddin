a1435fc071bded3b38f7ac7ea3562c9a0e025c4e
standon <standonjoyner@gmail.com>
2023-11-13 20:48:49 +0800

    添加项目文件。

---
+++ mateaddin/Class1.cs
@@ -0,0 +1,13 @@
+﻿using ExcelDna.Integration;
+
+namespace mateaddin
+{
+    public static class Functions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ mateaddin/RTD/RTDServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration.Rtd;
+
+namespace mateaddin.RTD
+{
+    [ComVisible(true)]
+    [ProgId(RTDServer.ProgId)]
+    internal class RTDServer : ExcelRtdServer
+    {
+        public const string ProgId = "mate.q";
+        protected override bool ServerStart()
+        {
+            return true;
+        }
+        protected override void ServerTerminate()
+        {
+        }
+        protected override object ConnectData(Topic topic, IList<string> topicInfo, ref bool newValues)
+        {
+            return "Hello " + topicInfo[1];
+        }
+        protected override void DisconnectData(Topic topic)
+        {
+        }
+        protected override int Heartbeat()
+        {
+            return 1;
+        }
+    }
+}
